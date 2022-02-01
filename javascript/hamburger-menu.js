ab62cd906de0f5c17bdb68a6577cefcf2e4878f1
anuakshat <53713926+anuakshat@users.noreply.github.com>
2022-02-01 22:05:42 +0530

    dropdown added with contents

--- javascript/hamburger-menu.js
+++ javascript/hamburger-menu.js
@@ -1,11 +1,18 @@
 const menuBtn = document.querySelector('.menu-btn');
+const dropDownContent=document.querySelector(".dropdown-content");
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
+    myDropdown.classList.toggle("show");
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
+  }
+  window.onclick=function(event){
+      if(!event.target.matches('menu-btn')){
+        openDropdown.classList.remove('show');
+      }
   }
 });
