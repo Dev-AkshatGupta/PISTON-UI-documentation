af592e0c5812bc0f6ac94a37df991976e9dd822d
anuakshat <53713926+anuakshat@users.noreply.github.com>
2022-02-02 08:00:11 +0530

    Hamburger menu added on small viewport

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// For highlight js
 {
     document.addEventListener('DOMContentLoaded', (event) => {
       document.querySelectorAll('pre code').forEach((el) => {
@@ -11,3 +12,37 @@
       })
     })
   }
+  // for hamburger menu button on small visualViewport
+
+  const menuBtn = document.querySelector('.menu-btn');
+const dropDownContent=document.querySelector(".dropdown-content");
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    myDropdown.classList.toggle("show");
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+ 
+});
+
+// For the dark and light mode
+
+const sun=document.querySelector("#light-mode");
+const sunIcon=document.querySelector(".fa-sun");
+const moon=document.querySelector("#dark-mode");
+sun.addEventListener("click",()=>{
+  console.log("fuck");
+});
+sunIcon.addEventListener("click",()=>{
+  console.log("icon");
+});
+
+
+
+
+
+
