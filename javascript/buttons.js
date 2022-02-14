56355a709d2b07fee165f34ae3d301da02ae83b8
anuakshat <53713926+anuakshat@users.noreply.github.com>
2022-02-14 13:18:33 +0530

    readme completed and also code indented properly

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,15 +1,15 @@
-const btnShow=document.querySelector("#btn-show");
-const btnNotShow=document.querySelector("#btn-not-show");
+const btnShow = document.querySelector("#btn-show");
+const btnNotShow = document.querySelector("#btn-not-show");
 
 // const btnRemove=document.querySelector("");
-btnShow.addEventListener("click",()=>{
-    console.log(2);
-  btnShow.disabled=true;
-  btnNotShow.style.display="block";
+btnShow.addEventListener("click", () => {
+  console.log(2);
+  btnShow.disabled = true;
+  btnNotShow.style.display = "block";
 })
-btnNotShow.addEventListener("click",()=>{
-    console.log(1);
-    btnNotShow.style.display="none";
-    btnShow.disabled=false;
-    btnShow.innerHtml=`Show`;
+btnNotShow.addEventListener("click", () => {
+  console.log(1);
+  btnNotShow.style.display = "none";
+  btnShow.disabled = false;
+  btnShow.innerHtml = `Show`;
 })
--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,20 +1,16 @@
-
-
-
-
 // for hamburger menu button on small visualViewport
 
 const menuBtn = document.querySelector('.menu-btn');
-const dropDownContent=document.querySelector(".dropdown-content");
+const dropDownContent = document.querySelector(".dropdown-content");
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-    myDropdown.classList.toggle("show");
-  if(!menuOpen) {
+  myDropdown.classList.toggle("show");
+  if (!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
   }
- 
+
 });
